5ff1f2aa8416020d2bb0c4160e929074dfc80b5a
Joe Winger <josephwinger1@gmail.com>
2020-04-21 16:20:29 -0400

    Add necessary mutations and getters

--- src/store.js
+++ src/store.js
@@ -5,19 +5,51 @@ Vue.use(Vuex);
 
 const room = {
 	namespaced: true,
-	
+
 	state: {
 		roomId: null,
 		gameState: null,
 		users: [],
 		currentBlackCard: null,
 		currentCzar: null,
+		currentTurnStatus: null,
 		activeCards: [],
 		pointsToWin: null,
 		turnWinningCard: null,
 		winner: null
 	},
-	mutations: {},
+	mutations: {
+		setRoomId(state, roomId) {
+			state.roomId = roomId;
+		},
+		setPointsToWin(state, numPointsToWin) {
+			state.numPointsToWin = numPointsToWin;
+		},
+		updateUsers(state, users) {
+			state.users = users;
+		},
+		updateGameState(state, newGameState) {
+			state.gameState = newGameState;
+		},
+		updateBlackCard(state, newBlackCard) {
+			state.currentBlackCard = newBlackCard;
+		},
+		updateCzar(state, newCzar) {
+			state.currentCzar = newCzar;
+		},
+		updateActiveCards(state, newActiveCards) {
+			state.activeCards = newActiveCards;
+		},
+		updateTurnStatus(state, newTurnStatus) {
+			state.turnStatus = newTurnStatus;
+		},
+		updateTurnWinningCard(state, card) {
+			state.turnWinningCard = card;
+		},
+		setGameWinner(state, username) {
+			state.winner = username;
+		}
+	},
 	actions: {},
 	getters: {}
 }
@@ -29,14 +61,38 @@ const user = {
 		isReady: false,
 		username: "",
 		hand: {},
-		isCzar: false,
 		isPrivileged: false,
 		playedThisTurn: false,
-		points: null
+		points: 0
 	},
-	mutations: {},
+	mutations: {
+		setUserName(state, username) {
+			state.username = username
+		},
+		setPrivileged(state, isPrivileged) {
+			state.isPrivileged = isPrivileged
+		},
+		setPlayedThisTurn(state, playedThisTurn) {
+			state.playedThisTurn = playedThisTurn
+		},
+		updateReadyStatus(state, readyInt) {
+			state.isReady = readyInt % 2 === 0;
+		},
+		updateHand(state, newHand) {
+			state.hand = newHand;
+		},
+		updatePoints(state, numPoints) {
+			state.points = numPoints
+		}
+		// removeCardFromHand(cardText): state.hand.filter(c => c != cardText) & update numCardsInHand
+	},
 	actions: {},
-	getters: {}
+	getters: {
+		isCzar(state, getters, rootState) {
+			return rootState.room.currentCzar === state.username
+		}
+		//isCzar
+	}
 }
 
 const store = new Vuex.Store({
