fd56b048e85fbd25bc0818685da00ed1cfae1755
Joe Winger <josephwinger1@gmail.com>
2020-04-21 14:33:24 -0400

    Add modules

--- src/store.js
+++ src/store.js
@@ -1,8 +1,22 @@
+const roomData = {
+	state: {},
+	mutations: {},
+	actions: {},
+	getters: {}
+}
+
+const userData = {
+	state: {},
+	mutations: {},
+	actions: {},
+	getters: {}
+}
+
 const store = new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  }
+	modules: {
+		roomData,
+		userData
+	}
 });
 
 export default store;
