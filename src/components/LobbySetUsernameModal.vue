4c8b322979beb8cdeadbaaddccbd48fcb2d56641
Joe Winger <josephwinger1@gmail.com>
2020-04-27 17:04:29 -0400

    Add vendor prefixes

--- src/components/LobbySetUsernameModal.vue
+++ src/components/LobbySetUsernameModal.vue
@@ -50,7 +50,11 @@ export default {
 
 	background-color: #4F4F4F64;
 	
-  transition: all 0.3s ease;
+	-webkit-transition: all 0.3s ease;
+	-moz-transition: all 0.3s ease;
+	-ms-transition: all 0.3s ease;
+	-o-transition: all 0.3s ease;
+	transition: all 0.3s ease;
 }
 #setUsernameModal {
 	display: flex;
